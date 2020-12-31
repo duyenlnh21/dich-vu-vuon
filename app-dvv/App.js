55533adf32538d51fc216713822a963306684fd3
lethiduyen000097 <duyenlt060097@gmail.com>
2020-12-31 11:10:16 +0700

    new app

---
+++ app-dvv/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Chào mừng quý bạn đến với dịch vụ vườn hay!</Text>
+      <Text>01/01/2021</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
