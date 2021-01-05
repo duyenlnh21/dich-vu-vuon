646b6302c494daf89e74b1a73b19e2219a81f7d6
lethiduyen000097 <duyenlt060097@gmail.com>
2021-01-05 15:28:58 +0700

    update hinh anh

--- app-dvv/App.js
+++ app-dvv/App.js
@@ -3,18 +3,14 @@ import PropTypes from "prop-types";
 import { View, Image } from "react-native";
 import styles from "./styles";
 
-export default function App({ reactSource, relaySource, senSource }) {
+export default function App({ reactSource, relaySource }) {
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={reactSource} />
       <Image style={styles.image} source={relaySource} />
-      <Image style={styles.image} source={senSource} />
     </View>
   );
 }
-
-
-
 
 const sourceProp = PropTypes.oneOfType([
   PropTypes.shape({
@@ -25,16 +21,12 @@ const sourceProp = PropTypes.oneOfType([
 
 App.propTypes = {
   reactSource: sourceProp,
-  relaySource: sourceProp,
-  senSource: sourceProp
-
+  relaySource: sourceProp
 };
 
 App.defaultProps = {
   reactSource: {
     uri: "https://facebook.github.io/react-native/docs/assets/favicon.png"
   },
-  relaySource: require("./images/relay.png"),
-  senSource: require("./images/sen.png")
-
+  relaySource: require("./images/relay.png")
 };
