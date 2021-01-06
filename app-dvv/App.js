3a227ed36f0595e53752019b4495990dbc5048ac
lethiduyen000097 <duyenlt060097@gmail.com>
2021-01-06 11:41:28 +0700

    update

--- app-dvv/App.js
+++ app-dvv/App.js
@@ -1,34 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { Text, View } from "react-native";
-import NetInfo from "@react-native-community/netinfo";
-import styles from "./styles";
+import React from "react";
+import { Text } from "native-base";
+import Container from "./Container";
 
-const connectedMap = {
-  none: "Disconnected",
-  unknown: "Disconnected",
-  wifi: "Connected",
-  cell: "Connected",
-  mobile: "Connected"
-};
-
-export default function App() {
-  const [connected, setConnected] = useState("");
-
-  useEffect(() => {
-    function onNetworkChange(connection) {
-      setConnected(connectedMap[connection.type]);
-    }
-
-    const unsubscribe = NetInfo.addEventListener(onNetworkChange);
-
-    return () => {
-      unsubscribe();
-    };
-  }, []);
-
+export default function App({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>{connected}</Text>
-    </View>
+    <Container title="Tiêu đề trang">
+      <Text>Nội dung trang </Text>
+    </Container>
   );
 }
