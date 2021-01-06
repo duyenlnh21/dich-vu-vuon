843a51b2c5f5aff411f13e737e61b509c069e267
lethiduyen000097 <duyenlt060097@gmail.com>
2021-01-06 15:30:05 +0700

    start dự án  Dịch vụ vườn - Lê Nguyễn Hữu Duyên

--- app-dvv/Contact.js
+++ app-dvv/Contact.js
@@ -5,7 +5,13 @@ import Container from "./Container";
 export default function Contact({ navigation }) {
   return (
     <Container navigation={navigation}>
-      <Text>Nội dung của Contact...</Text>
+      <Text>Dịch vụ vườn - Lê Nguyễn Hữu Duyên</Text>
+      <Text>Sinh ra và lớn lên ở quê lúa Thái Bình, Lê Nguyễn Hữu Duyên tên thật là Lê Thị Duyên sinh năm 1997.</Text>
+      <Text>Là sinh viên khoa Công nghệ thông tin Trường Đại học Thủy Lợi, trong thời sinh viên một lần cô được nghe người thầy của mình nói về IoT trong nông nghiêp.</Text>
+      <Text>Với những năm tháng tuổi thơ sống ở quê hương với nghề làm nông của gia đình, cộng thêm vài năm là sinh viên khoa CNTT ở Thành phố lớn đã giúp cô có thêm khao khát làm IoT cho nông nghiệp, nói cách thời đại hơn thì là "Nông nghiệp thông minh".</Text>
+      <Text>Vẫn tiếp tục sự nghiệp học hành, vẫn theo đuổi ước mơ thành một Nữ lập trình viên, lại thêm áp lực cuộc sống nơi thành phố của đứa con gái nhà quê gia đình làm nông, giúp cô trở nên phải cố gắng nhiều hơn, theo cách đặc biệt hơn .</Text>
+      <Text>Cũng một phần của may mắn là sự luôn dữ mong muốn trong mình, cộng không ngừng tìm kiếm những cơ hội. Ngày 06/01/2021 Lê Nguyễn Hữu Duyên đã có thể bắt đầu phát triển dự án của mình .</Text>
+      <Text>Dịch vụ vườn - Lê Nguyễn Hữu Duyên đã ra đời như thế</Text>
     </Container>
   );
 }
